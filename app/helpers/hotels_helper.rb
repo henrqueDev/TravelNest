0385ab0e6310422aa5af29b1f7bd577d4d7c3088
henrqueDev <lhdeveloper137@gmail.com>
2023-10-13 11:29:27 -0300

    adicionar testes + config rspec #2

--- app/helpers/hotels_helper.rb
+++ app/helpers/hotels_helper.rb
@@ -1,2 +1,20 @@
 module HotelsHelper
+    def evaluation_description(evaluation)
+        case evaluation
+        when 0..1.999999999
+            "Muito Ruim"
+        when 2..5.999999999
+            "Ruim"
+        when 6..6.999999999
+            "Razoavel"
+        when 7..7.999999999
+            "Bom"
+        when 8..8.999999999
+            "Muito bom"
+        when 9..10
+            "Extraordinário"
+        else
+            "You gave me #{evaluation} -- I have no idea what to do with that."
+        end
+    end
 end
