51d310e6dca9f6fa3d520b445a268381b71d67ae
joaopedroRodriguel <rodrigues.sousa@academico.ifpb.edu.br>
2023-11-09 08:07:25 -0300

    implementacao de filtros

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'homepage', to: 'homepage#index'
+  get 'filter/get_states'
+  get 'filter/get_cities'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
